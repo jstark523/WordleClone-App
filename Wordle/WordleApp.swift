9c74a60abf8edb0967c9f12029e7cd1cc1b93c65
Jason Starkman <jrstarkman@gmail.com>
2023-01-30 17:26:52 -0500

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Jason Starkman on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
